361865fcc20cc0cf0b3c407afc0d526741b6237d
Azic-rello <github1akk2009@gmail.com>
2025-12-19 15:25:41 +0500

    create

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import User from "../components/User";
+import Phone from "../components/Phone";
+import Info from "../components/Info";
+import Cards from "../components/Cards";
+import Futer from "../components/Futer";
+
+const Home = () => {
+  return
+  <div>
+    <Navbar/>
+    <Hero/>
+    <User />
+    <Phone />
+    <Info/>
+    <Cards/>
+    <Futer/>
+  </div>;
+};
+
+export default Home;
